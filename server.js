2d2d892572bca436c28708512b70bef0c631a846
MrTanoshii <47116127+MrTanoshii@users.noreply.github.com>
2022-08-10 15:28:04 +0400

    feat: init server

---
+++ server.js
@@ -0,0 +1,10 @@
+var http = require("http");
+
+console.log("Server running at http://localhost:8080/");
+
+http
+  .createServer(function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end("Hello World!");
+  })
+  .listen(8080);
